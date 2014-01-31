f9026ce24a1f05c7dcc7b3d5524f7f26e7ad3e4f
Kevin Murray <spam@kdmurray.id.au>
2014-01-31 14:41:42 +1100

    add basic module structure

---
+++ tests/helpers.py
@@ -0,0 +1,9 @@
+
+# SKIP messages
+SKIP_NEED_INTERNET = "Test requires internet access"
+SKIP_NEED_LINUX = "Test must be run on a linux-based system"
+SKIP_NEED_MACOSX = "Test must be run on a Mac OSX system"
+SKIP_NEED_WINDOWS = "Test must be run on a Windows system"
+
+SKIP_NEED_FILE = "Test requires file {0}"
+
