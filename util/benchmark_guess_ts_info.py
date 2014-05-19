1f0e1f41a00edf535f0e06c3cc19bddd3daa62a2
Kevin Murray <spam@kdmurray.id.au>
2014-05-19 14:54:15 +1000

    [benchmark_guess_ts_info] use timeit module

--- util/benchmark_guess_ts_info.py
+++ util/benchmark_guess_ts_info.py
@@ -1,9 +1,15 @@
 from timestream.parse import guess_manifest_info
 from sys import argv, stdout
 import json
+import timeit
 
 def main():
-    json.dump(_guess_manifest_info(argv[1]), stdout, indent=2)
+    n = 10
+    cmd = "from timestream.parse import guess_manifest_info;"
+    cmd += "from sys import argv; guess_manifest_info(argv[1])"
+    time = timeit.timeit(cmd, number=n) / float(n)
+    print("guess_manifest_info took {}s per iteration".format(time))
+    json.dump(guess_manifest_info(argv[1]), stdout, indent=2)
 
 if __name__ == "__main__":
     main()
