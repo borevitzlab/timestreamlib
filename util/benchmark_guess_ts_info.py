6591186c5179b96d5c5d2c1c6c922e45e9be314f
Kevin Murray <spam@kdmurray.id.au>
2014-06-04 13:10:00 +1000

    [util] fix benchmark_guess_ts_info

--- util/benchmark_guess_ts_info.py
+++ util/benchmark_guess_ts_info.py
@@ -1,4 +1,4 @@
-from timestream.parse import guess_manifest_info
+from timestream.parse import ts_guess_manifest
 from sys import argv, stdout
 import json
 import timeit
@@ -9,7 +9,7 @@ def main():
     cmd += "from sys import argv; guess_manifest_info(argv[1])"
     time = timeit.timeit(cmd, number=n) / float(n)
     print("guess_manifest_info took {}s per iteration".format(time))
-    json.dump(guess_manifest_info(argv[1]), stdout, indent=2)
+    json.dump(ts_guess_manifest(argv[1]), stdout, indent=2)
 
 if __name__ == "__main__":
     main()
