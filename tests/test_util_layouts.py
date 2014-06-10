b7a1f2ba5aa6032705871b490b0d930e76783d98
Kevin Murray <spam@kdmurray.id.au>
2014-06-10 11:36:54 +1000

    [ts.util.layouts] add layouts module

---
+++ tests/test_util_layouts.py
@@ -0,0 +1,29 @@
+from unittest import TestCase, skip, skipIf, skipUnless
+
+from timestream.util import (
+    layouts,  # module
+)
+
+
+class TestTrayPosToChamberIndex(TestCase):
+    _multiprocess_can_split_ = True
+    maxDiff = None
+
+    def test_tray_pos_to_chamber_idx(self):
+        """Tests for ts.util.traypos_to_chamber_index"""
+        self.assertEqual(layouts.traypos_to_chamber_index("1A1"), 1)
+        self.assertEqual(layouts.traypos_to_chamber_index("01A1"), 1)
+        self.assertEqual(layouts.traypos_to_chamber_index("1a1"), 1)
+        self.assertEqual(layouts.traypos_to_chamber_index("01a1"), 1)
+        self.assertEqual(layouts.traypos_to_chamber_index("09A1"), 161)
+        self.assertEqual(layouts.traypos_to_chamber_index("16D4"), 319)
+        with self.assertRaises(TypeError):
+            layouts.traypos_to_chamber_index(321)
+        with self.assertRaises(ValueError):
+            layouts.traypos_to_chamber_index("234A3")
+        with self.assertRaises(ValueError):
+            layouts.traypos_to_chamber_index("24A33")
+        with self.assertRaises(ValueError):
+            layouts.traypos_to_chamber_index("24A")
+        with self.assertRaises(ValueError):
+            layouts.traypos_to_chamber_index("A2")
