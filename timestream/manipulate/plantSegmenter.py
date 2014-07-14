Fix Bug. Make a copy of image when masking
We were chaning the main pot image. We now make a copy to put mask on a copy
of the main image.
* timestream/manipulate/pipecomponents.py (__call__): Use & instead of | when
masking
* timestream/manipulate/plantSegmenter.py (maskedImage): Make a copy of
self.si with self.si.copy()
(show): Use & instead of |

timestream/manipulate/plantSegmenter.py
@@ -333,7 +333,7 @@ class ImagePotHandler(object):
         retVal = np.reshape(retVal, (height, width, dims), order="F")
 
         if inSuper:
-            superI = self.si
+            superI = self.si.copy()
             superI[self._rect[1]:self._rect[3], \
                        self._rect[0]:self._rect[2], :] = retVal
             retVal = superI
@@ -485,7 +485,7 @@ class ImagePotMatrix(object):
         sImage = self.image
         for tray in self.its:
             for pot in tray.pots:
-                sImage = sImage | pot.maskedImage(inSuper=True)
+                sImage = sImage & pot.maskedImage(inSuper=True)
 
         plt.figure()
         plt.imshow(sImage.astype(np.uint8))