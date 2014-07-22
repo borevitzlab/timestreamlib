borevitzlab/timestreamlib

commit 1717e0100369cd0b78530b4bf0fa279565dd02a3

add writing positions of tray and pot to timestream

timestream/manipulate/pipecomponents.py
@@ -23,7 +23,7 @@ import numpy as np
 import cv2
 import timestream.manipulate.correct_detect as cd
 import timestream.manipulate.plantSegmenter as ps
-from timestream import TimeStreamImage
+from timestream import TimeStreamImage, parse
 import timestream
 import os
 import os.path
@@ -260,15 +260,6 @@ class ImageColorCorrector ( PipeComponent ):
             self.imageCorrected = image
         self.image = image # display
 
-        # save processed image if selected so
-        if self.writeImage:
-            print('   Save corrected image')
-            img = timestream.TimeStreamImage()
-            img.datetime = context["img"].datetime
-            img.pixels = self.imageCorrected
-            img.data["processed"] = "yes"
-            context["wts"].write_image(img)
-
         return([self.imageCorrected])
 
     def show(self):
@@ -329,6 +320,13 @@ class TrayDetector ( PipeComponent ):
                     "Low tray matching score. Likely tray %d is missing." %i)
 
             self.trayLocs.append(loc)
+
+        # add tray location information
+        date = context["img"].datetime
+        for outstream in context["outstreams"]:
+            ts_out = context[outstream["name"]]
+            ts_out.image_data[parse.ts_format_date(date)]["trayLocs"] = self.trayLocs
+            ts_out.write_metadata()
 
         return([self.image, self.imagePyramid, self.trayLocs])
 
@@ -425,6 +423,13 @@ class PotDetector ( PipeComponent ):
                     potLocs_.append(estimateLoc)
             self.potLocs2.append(potLocs)
             self.potLocs2_.append(potLocs_)
+
+        # add pot location information
+        date = context["img"].datetime
+        for outstream in context["outstreams"]:
+            ts_out = context[outstream["name"]]
+            ts_out.image_data[parse.ts_format_date(date)]["potLocs"] = self.potLocs2
+            ts_out.write_metadata()
 
         return([self.image, self.potLocs2])
 

commit 12e67f46d1ac0d1a647889495ef75b225d62c1db

Fix bug introduced in 1717e01
1717e01 put pot positions and tray positions in the output timestream metadata.
It would fail when `ts_out.image_data[parse.ts_format_date(date)]` was called
and the image was not there because it had not been written yet.

Relevant Traceback:
ceback (most recent call last):
  File "pipeline_demo.py", line 106, in <module>
    result = pl.process(context, [img.pixels], visualise)
  File "build/bdist.linux-x86_64/egg/timestream/manipulate/pipeline.py", line 82, in process
  File "build/bdist.linux-x86_64/egg/timestream/manipulate/pipecomponents.py", line 328, in __call__
KeyError: '2014_06_19_09_00_00'

The bug would only be seen on pipelines that called traydetector or potdetector
without calling resultingimagewriter.

* timestream/manipulate/pipecomponents.py (TrayDetector.__call__): Add relevant
value to context["outputwithimage"] so it can be output when the image is
written.
* timestream/manipulate/pipecomponents.py (PotDetector.__call__): Likewise
* timestream/manipulate/pipecomponents.py (ResultingImageWriter.__call__): Take
all values in context["outputwithimage"] and write them as metadata.
* util/pipeline_demo.py: Initializle context["outputwithimage"] to an empty dict.
* util/pipeline_demo_correct_images_daytime.py: Likewise

timestream/manipulate/pipecomponents.py
@@ -323,10 +323,7 @@ class TrayDetector ( PipeComponent ):
 
         # add tray location information
         date = context["img"].datetime
-        for outstream in context["outstreams"]:
-            ts_out = context[outstream["name"]]
-            ts_out.image_data[parse.ts_format_date(date)]["trayLocs"] = self.trayLocs
-            ts_out.write_metadata()
+        context["outputwithimage"]["trayLocs"] = self.trayLocs
 
         return([self.image, self.imagePyramid, self.trayLocs])
 
@@ -426,10 +423,7 @@ class PotDetector ( PipeComponent ):
 
         # add pot location information
         date = context["img"].datetime
-        for outstream in context["outstreams"]:
-            ts_out = context[outstream["name"]]
-            ts_out.image_data[parse.ts_format_date(date)]["potLocs"] = self.potLocs2
-            ts_out.write_metadata()
+        context["outputwithimage"]["potLocs"] = self.potLocs2
 
         return([self.image, self.potLocs2])
 
@@ -690,6 +684,11 @@ class ResultingImageWriter ( PipeComponent ):
         img.datetime = context["img"].datetime
         img.pixels = args[0]
         img.data["processed"] = "yes"
-        context[self.outstream].write_image(img)
+        for key, value in context["outputwithimage"].iteritems():
+            img.data[key] = value
+
+        ts_out = context[self.outstream]
+        ts_out.write_image(img)
+        ts_out.write_metadata()
 
         return (args)

util/pipeline_demo.py
@@ -62,6 +62,10 @@ for outstream in outstreams:
 
 context["outputroot"] = outputRootPath
 
+# Dictionary where we put all values that should be added with an image as soon
+# as it is output with the TimeStream
+context["outputwithimage"] = {}
+
 # initialise processing pipeline
 pl = pipeline.ImagePipeline(ts.data["settings"], context)
 

util/pipeline_demo_correct_images_daytime.py
@@ -68,7 +68,6 @@ for attr in timestream.parse.validate.TS_MANIFEST_KEYS:
     print("   ts.%s:" % attr, getattr(ts, attr))
 
 #create new timestream for output data
-context["outstreams"] = outstreams
 for outstream in outstreams:
     ts_out = timestream.TimeStream()
     ts_out.data["settings"] = settings
@@ -85,8 +84,11 @@ for outstream in outstreams:
     context[outstream["name"]] = ts_out
     print("   ts_out.path:", ts_out.path)
 
-
 context["outputroot"] = outputRootPath
+
+# Dictionary where we put all values that should be added with an image as soon
+# as it is output with the TimeStream
+context["outputwithimage"] = {}
 
 # initialise processing pipeline
 pl = pipeline.ImagePipeline(ts.data["settings"], context)