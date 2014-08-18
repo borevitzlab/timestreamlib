98209bb6f4fa3137264318fd635d1520fe4e9232
Chuong Nguyen <chuong.v.nguyen@gmail.com>
2014-08-18 11:27:09 +1000

    Improve options for date and time ranges

--- timestreamCLI/playTimestream.py
+++ timestreamCLI/playTimestream.py
@@ -9,25 +9,46 @@ from __future__ import absolute_import, division, print_function
 import cv2
 import logging
 import timestream
-import sys
 import numpy as np
+from timestream.parse import ts_parse_date
+import docopt
+import datetime
+
+CLI_OPTS = """
+USAGE:
+    playtTimestream.py -i IN [-d DELAY] [--int INTERVAL] [-s START] [-e END] [--sh STARTHOUR] [--eh ENDHOUR]
+
+OPTIONS:
+    -i IN          Input timestream directory
+    -d DELAY       Playing time delay [500 msecs]
+    --int INTERVAL Looping time interval [24*60*60 mins]
+    -s START       Start date and time of looping
+    -e END         End  date and time of looping
+    --sh STARTHOUR Start time range of looping
+    --eh ENDHOUR   End time range of looping
+"""
+opts = docopt.docopt(CLI_OPTS)
 
 interval = 24*60*60 #None
 start = None
 end = None
+start_hour = None
+end_hour = None
 delay = 500
-if len(sys.argv) < 2:
-    inputRootPath = '/home/chuong/Data/phenocam/a_data/TimeStreams/Borevitz/BVZ0018/BVZ0018-GC05B-C01~fullres-unr'
-else:
-    try:
-        inputRootPath = sys.argv[1]
-        delay = int(sys.argv[2])
-        interval = int(sys.argv[3])
-        start = sys.argv[4]
-        end = sys.argv[5]
-    except:
-        pass
-    
+inputRootPath = opts['-i']
+if opts['-d']:
+    delay = int(opts['-d'])
+if opts['--int']:
+    interval = int(opts['-int'])
+if opts['-s']:
+    start = ts_parse_date(opts['-s'])
+if opts['-e']:
+    end = ts_parse_date(opts['-e'])
+if opts['--sh']:
+    start_hour = datetime.time(int(opts['--sh']), 0, 0)
+if opts['--eh']:
+    end_end = datetime.time(int(opts['--eh']), 0, 0)
+
 timestream.setup_module_logging(level=logging.INFO)
 ts = timestream.TimeStream()
 ts.load(inputRootPath)
@@ -35,7 +56,8 @@ ts.load(inputRootPath)
 windowName = 'image'
 cv2.imshow(windowName, np.zeros([100,100], dtype = np.uint8))
 cv2.moveWindow(windowName, 10,10)
-for img in ts.iter_by_timepoints(start=start, end=end, interval = interval):
+for img in ts.iter_by_timepoints(start=start, end=end, interval = interval,
+                                 start_hour = start_hour, end_hour = end_hour):
     if img is None or img.pixels is None:
         continue
     # estimate a downscale factor to make image fit into a normal HD screen
