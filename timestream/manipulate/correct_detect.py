c358f29cb71dbc07ff9cc5bc946928a9976d6d58
Joel Andres Granados <joel.granados@gmail.com>
2014-08-28 09:00:39 +0200

    Remove incomplete comment

--- timestream/manipulate/correct_detect.py
+++ timestream/manipulate/correct_detect.py
@@ -611,7 +611,6 @@ def createImagePyramid(Image, NoLevels=5):
 def matchTemplatePyramid(PyramidImages, PyramidTemplates, RotationAngle=None,
                          EstimatedLocation=None, SearchRange=None, NoLevels=4,
                          FinalLevel=1):
-    # TODO: KDM - I'm not so 
     for i in range(NoLevels - 1, -1, -1):
         if i == NoLevels - 1:
             if EstimatedLocation is None:
