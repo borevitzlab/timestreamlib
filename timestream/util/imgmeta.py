7ed3416df40a6bb7f3bafbdcb5864b0cb8a251f7
Kevin Murray <spam@kdmurray.id.au>
2014-09-15 16:22:32 +1000

    remove dependency on Wand

--- timestream/util/imgmeta.py
+++ timestream/util/imgmeta.py
@@ -21,11 +21,7 @@
 .. moduleauthor:: Kevin Murray <spam@kdmurray.id.au>
 """
 import datetime
-try:
-    import exifread as er
-    library = "exifread"
-except ImportError:
-    library = "wand"
+import exifread as er
 from string import (
     digits,
 )
@@ -46,35 +42,19 @@ def get_exif_tags(image, mode="silent"):
     """
     if mode not in {"silent", "raise"}:
         raise ValueError("Bad get_exif_tags mode '{}'".format(mode))
-    if library == "wand":
-        # use the wand library, as either we've been told to or the faster
-        # exifread isn't available
-        from wand.image import Image
-        with Image(filename=image) as img:
-            exif = {k[5:]: v for k, v in img.metadata.items() if
-                    k.startswith('exif:')}
-    elif library == "exifread":
-        try:
-            er.__doc__
-        except NameError:
-            import exifread as er
-        with open(image, "rb") as fh:
-            tags = er.process_file(fh, details=False)
-        tags = dict_unicode_to_str(tags)
-        # remove the first bit off the tags
-        exif = {}
-        for k, v in tags.items():
-            # Remove the EXIF/Image category from the keys
-            k = " ".join(k.split(" ")[1:])
-            # weird exif tags in CR2s start with a 2/3, or have hex in them
-            if k[0] in digits or "0x" in k:
-                continue
-            v = str(v)
-            exif[k] = v
-    else:
-        raise ValueError(
-            "Library '{}' not supported (only wand and exifread are".format(
-                library))
+    with open(image, "rb") as fh:
+        tags = er.process_file(fh, details=False)
+    tags = dict_unicode_to_str(tags)
+    # remove the first bit off the tags
+    exif = {}
+    for k, v in tags.items():
+        # Remove the EXIF/Image category from the keys
+        k = " ".join(k.split(" ")[1:])
+        # weird exif tags in CR2s start with a 2/3, or have hex in them
+        if k[0] in digits or "0x" in k:
+            continue
+        v = str(v)
+        exif[k] = v
     return exif
 
 
