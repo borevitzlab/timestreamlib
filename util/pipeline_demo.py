7fa010be2d5ce9e94f5fae5861d1cf82ecde4276
Joel Andres Granados <joel.granados@gmail.com>
2014-09-24 10:46:28 +0200

    Add GPL comments to the pipeline_demo file.
    * util/pipeline_demo.py: $subject.

--- util/pipeline_demo.py
+++ util/pipeline_demo.py
@@ -1,9 +1,23 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Jun 25 13:31:54 2014
+# Copyright (C) 2014
+# Author(s): Chuong Nguyen <chuong.v.nguyen@gmail.com>
+#            Joel Granados <joel.granados@gmail.com>
+#            Kevin Murray <kevin@kdmurray.id.au>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-@author: chuong nguyen, chuong.v.nguyen@gmail.com
-"""
 from __future__ import absolute_import, division, print_function
 
 import docopt
